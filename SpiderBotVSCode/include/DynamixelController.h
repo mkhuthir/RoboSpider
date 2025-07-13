437d29704998cef301483165f285e8c73baa61d2
mkhuthir <mkhuthir@gmail.com>
2025-07-13 20:34:26 +0400

    u

--- SpiderBotVSCode/include/DynamixelController.h
+++ SpiderBotVSCode/include/DynamixelController.h
@@ -1,32 +1,30 @@
 #ifndef DYNAMIXELCONTROLLER_H
 #define DYNAMIXELCONTROLLER_H
 
-#include <DynamixelWorkbench.h> // Include DynamixelWorkbench for managing servos
-#include <vector>               // Include vector for handling lists of servos
+    #include <DynamixelWorkbench.h> // Include DynamixelWorkbench for managing servos
 
+    class DynamixelController {
+        public:
+            DynamixelController();
+            bool begin(const char* device_name, uint32_t baudrate); // initialize the controller with device name and baudrate
+            bool ping(uint8_t dxl_id);                              // ping a servo to check if it is connected
+            bool jointMode(uint8_t dxl_id);                         // set a servo to joint mode
+            bool initServo(uint8_t dxl_id);                         // initialize a servo with default settings
+        
+            bool goalPosition(uint8_t dxl_id, int32_t position);    // set the goal position of a servo
+        
+            bool playMelody();                                      // play a melody using the servos
+            float getBatteryVoltage();                              // get the battery voltage
+            
+            DynamixelWorkbench* getWorkbench();                     // if you need to expose the workbench pointer
 
-class DynamixelController {
-public:
-    DynamixelController();
-    bool begin(const char* device_name, uint32_t baudrate);                                             // initialize the controller with device name and baudrate
-    bool ping(uint8_t dxl_id);                                                                          // ping a servo to check if it is connected
-    bool jointMode(uint8_t dxl_id);                                                                     // set a servo to joint mode
-    bool initServo(uint8_t dxl_id);                                                                     // initialize a servo with default settings
-   
-    bool goalPosition(uint8_t dxl_id, int32_t position);                                                // set the goal position of a servo
-   
-    bool playMelody();                                                                                  // play a melody using the servos
-    float getBatteryVoltage();                                                                          // get the battery voltage
-    
-    DynamixelWorkbench* getWorkbench();                                                                 // if you need to expose the workbench pointer
+        private:
+            DynamixelWorkbench  dxl_wb;             // DynamixelWorkbench instance for managing servos
+            const char          *log;               // Log string for debugging 
+            bool                result = false;     // Result of operations
+            uint16_t            model_number = 0;   // Model number of the servo being operated on
+        
 
-private:
-    DynamixelWorkbench  dxl_wb;             // DynamixelWorkbench instance for managing servos
-    const char          *log;               // Log string for debugging 
-    bool                result = false;     // Result of operations
-    uint16_t            model_number = 0;   // Model number of the servo being operated on
-   
-
-};
+    };
 
 #endif // DYNAMIXELCONTROLLER_H
--- SpiderBotVSCode/src/DynamixelController.cpp
+++ SpiderBotVSCode/src/DynamixelController.cpp
@@ -82,11 +82,8 @@ bool DynamixelController::goalPosition(uint8_t dxl_id, int32_t position) {
 }
 
 float DynamixelController::getBatteryVoltage() {
-    int adc_value;
-    float vol_value;
-    adc_value = analogRead(BDPIN_BAT_PWR_ADC);              // Read the ADC value from the battery power pin
-    vol_value = map(adc_value, 0, 1023, 0, 330*57/10)/100;  // Convert ADC value to voltage (assuming 3300mV reference and 57:10 voltage divider)
-    return vol_value;                                       // Return the battery voltage
+    int adc_value = analogRead(BDPIN_BAT_PWR_ADC);          // Read the ADC value from the battery power pin
+    return (map(adc_value, 0, 1023, 0, 330*57/10)/100.0);   // Convert ADC value to voltage (assuming 3300mV reference and 57:10 voltage divider)
 }
 
 // Play a melody using OpenCR Buzzer
