c843ecb59ad909b814f1f93ced2bafea53a7917d
mkhuthir <mkhuthir@gmail.com>
2025-07-19 04:13:03 +0400

    u

--- SpiderBotVSCode/include/GaitController.h
+++ SpiderBotVSCode/include/GaitController.h
@@ -13,24 +13,24 @@
 
     class GaitController {
         public:
-            GaitController();                               // Constructor
-            void init(Hexapod* hexapod);                    // Initialize with Hexapod
-            void setGait(GaitType newGait);                 // Set the current gait type
-            GaitType getGait() const;                       // Get the current gait type
-            void setStepInterval(unsigned long interval);   // Set the step interval
-            unsigned long getStepInterval() const;          // Get the step interval
-            void update();                                  // Update the gait controller
+            GaitController();                                           // Constructor
+            void            init(Hexapod* hexapod);                     // Initialize with Hexapod
+            void            setGait(GaitType newGait);                  // Set the current gait type
+            GaitType        getGait() const;                            // Get the current gait type
+            void            setStepInterval(unsigned long interval);    // Set the step interval
+            unsigned long   getStepInterval() const;                    // Get the step interval
+            void            update();                                   // Update the gait controller
 
         private:
-            Hexapod*        robot;                          // Pointer to the Hexapod instance
-            GaitType        gait;                           // Current gait type
-            unsigned long   lastUpdate;                     // Last update time
-            int             currentPhase;                   // Current phase of the gait
-            unsigned long   stepInterval;                   // Interval between steps
+            Hexapod*        robot;                                      // Pointer to the Hexapod instance
+            GaitType        gait;                                       // Current gait type
+            unsigned long   lastUpdate;                                 // Last update time
+            int             currentPhase;                               // Current phase of the gait
+            unsigned long   stepInterval;                               // Interval between steps
 
-            void doWaveGait();                              // Perform the wave gait
-            void doRippleGait();                            // Perform the ripple gait
-            void doTripodGait();                            // Perform the tripod gait
+            void            doWaveGait();                               // Perform the wave gait
+            void            doRippleGait();                             // Perform the ripple gait
+            void            doTripodGait();                             // Perform the tripod gait
     };
 
 #endif // GaitController_h
