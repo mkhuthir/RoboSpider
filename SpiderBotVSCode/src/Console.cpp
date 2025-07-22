1d3f12fb688a0080b796e0f98e1d1789f8deaa17
mkhuthir <mkhuthir@gmail.com>
2025-07-22 22:31:24 +0400

    u

--- SpiderBotVSCode/src/Console.cpp
+++ SpiderBotVSCode/src/Console.cpp
@@ -59,7 +59,15 @@ void Console::processCommand(const String& command) {
         con.println("  help/h/?  - Show this help message");
         con.println("  cls       - Clear the terminal screen");
         con.println("  status    - Show current status of the system");
-        
+        con.println("  t-up      - Turret up");
+        con.println("  t-down    - Turret down");
+        con.println("  t-left    - Turret left");
+        con.println("  t-right   - Turret right");
+        con.println("  t-home    - Turret home position");
+        con.println("  g-wave    - Start wave gait");
+        con.println("  g-ripple  - Start ripple gait");
+        con.println("  g-tripod  - Start tripod gait");
+        con.println("  g-idle    - Start idle gait");
 
     } else if (command == "cls") {                      
         con.print("\033[2J\033[H");                     // ANSI escape code to clear screen and move cursor to home
@@ -69,6 +77,33 @@ void Console::processCommand(const String& command) {
         hexapod->printStatus(con);
         turret->printStatus(con);
         gc->printStatus(con);
+
+    } else if (command == "t-up") {
+        turret->moveUp();
+
+    } else if (command == "t-down") {
+        turret->moveDown();
+
+    } else if (command == "t-left") {
+        turret->moveLeft();
+
+    } else if (command == "t-right") {
+        turret->moveRight();
+        
+    } else if (command == "t-home") {
+        turret->moveHome();
+
+    } else if (command == "g-wave") {
+        gc->setGait(GAIT_WAVE);
+
+    } else if (command == "g-ripple") {
+        gc->setGait(GAIT_RIPPLE);
+
+    } else if (command == "g-tripod") {
+        gc->setGait(GAIT_TRIPOD);
+
+    } else if (command == "g-idle") {
+        gc->setGait(GAIT_IDLE);
 
     } else {
         con.println("[Error] Unknown command: " + command);
