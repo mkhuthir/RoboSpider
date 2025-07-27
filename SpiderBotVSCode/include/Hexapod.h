38b67b34b34fb407ed9e47235757ee518b510e6f
mkhuthir <mkhuthir@gmail.com>
2025-07-27 05:06:41 +0400

    u

--- SpiderBotVSCode/include/Hexapod.h
+++ SpiderBotVSCode/include/Hexapod.h
@@ -14,13 +14,13 @@
       void printStatus(Stream& stream);       // Print the status of all legs
   
     private:
-      static constexpr uint8_t handler_index  = 0;                           // Index for sync write handler
-      static constexpr uint8_t HEXAPOD_LEGS   = 6;                           // Maximum number of legs
-      static constexpr uint8_t LEG_SERVOS     = 3;                           // Number of servos per leg
-      static constexpr uint8_t HEXAPOD_SERVOS = HEXAPOD_LEGS * LEG_SERVOS;   // Maximum number of servos
+      static constexpr uint8_t  handler_index  = 0;                           // Index for sync write handler
+      static constexpr uint8_t  HEXAPOD_LEGS   = 6;                           // Maximum number of legs
+      static constexpr uint8_t  LEG_SERVOS     = 3;                           // Number of servos per leg
+      static constexpr uint8_t  HEXAPOD_SERVOS = HEXAPOD_LEGS * LEG_SERVOS;   // Maximum number of servos
 
-      Leg                   legs[HEXAPOD_LEGS];                              // Array of legs
-      Servo*                servo;                                           // Dynamixel controller instance
+      Leg                       legs[HEXAPOD_LEGS];                           // Array of legs
+      Servo*                    servo;                                        // Dynamixel controller instance
   };
 
 #endif // HEXAPOD_H
