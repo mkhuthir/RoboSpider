9278df826ae6236a8337d1367dbbb52d001600c0
mkhuthir <mkhuthir@gmail.com>
2025-08-17 13:12:09 +0400

    u

--- code/DXLDriver.cpp
+++ code/DXLDriver.cpp
@@ -1,28 +1,4 @@
 #include "DXLDriver.h"
 
-DXLDriver::DXLDriver() : log_level(0) {}
 
-DXLDriver::~DXLDriver() {}
-
-bool DXLDriver::begin(const char* device_name, uint32_t baudrate) {
-    if (!dxl.begin(device_name, baudrate)) {
-        return false;
-    }
-    return true;
-}
-
-void DXLDriver::setLogLevel(uint8_t level) {
-    log_level = level;
-}
-
-void DXLDriver::setPacketHandler(float protocol_version) {
-    dxl.setPacketHandler(protocol_version);
-}
-
-float DXLDriver::getProtocolVersion(void) {
-    return dxl.getProtocolVersion();
-}
-
-uint32_t DXLDriver::getBaudrate(void) {
-    return dxl.getBaudrate();
-}
+// end of DXLDriver.cpp
--- code/DXLDriver.h
+++ code/DXLDriver.h
@@ -1,23 +1,15 @@
 #ifdef DXL_DRIVER_H
 #define DXL_DRIVER_H
 
-#include <Arduino.h>
-#include <DynamixelSDK.h>
+    #include <Arduino.h>
+    #include <DynamixelSDK.h>
 
-class DXLDriver {
-public:
-    DXLDriver();
-    ~DXLDriver();
+    class DXLDriver {
 
-    bool begin(const char* device_name, uint32_t baudrate);
-    void setLogLevel(uint8_t level);
-    void setPacketHandler(float protocol_version);
-    float getProtocolVersion(void);
-    uint32_t getBaudrate(void);
+    public:
 
-private:
-    DynamixelSDK dxl;
-    uint8_t log_level;
-};
+    private:
+
+    };
 
 #endif
